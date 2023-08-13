cd87a4e0fc5efa452974a1ba8b6d65a29d13372c
djon198360 <le1907@mail.ru>
2023-08-13 04:11:45 +0300

    fix format prettier

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,25 +1,21 @@
 function NotFoundRender() {
-    return(
-        <div className="container-enter">
-        <div className="modal__block">
-            
-          <form className="modal__form-login" action="#">
-          
-            <a href="../">
-              <div className="modal__logo">
-                <img src="../img/logo_modal.png" alt="logo" />
-              </div>
-            </a>
-            <h1 className="modal__error_title">Такой страницы нет</h1>
-            <button type="button" className="modal__btn-enter">
-              <a href="/">Перейти на главную</a>
-            </button>
-
-          </form>
-        </div>
+  return (
+    <div className="container-enter">
+      <div className="modal__block">
+        <form className="modal__form-login" action="#">
+          <a href="../">
+            <div className="modal__logo">
+              <img src="../img/logo_modal.png" alt="logo" />
+            </div>
+          </a>
+          <h1 className="modal__error_title">Такой страницы нет</h1>
+          <button type="button" className="modal__btn-enter">
+            <a href="/">Перейти на главную</a>
+          </button>
+        </form>
       </div>
-
-    )
+    </div>
+  );
 }
 
 export default NotFoundRender;
