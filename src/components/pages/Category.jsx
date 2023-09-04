c557d9c94c076393bcd3e4c10d47dc9526692b7a
djon198360 <114354138+djon198360@users.noreply.github.com>
2023-09-04 23:21:44 +0300

    error updte token

---
+++ src/components/pages/Category.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import NavMenuLeftRender from "../NavLeft/NavLeft";
+import SearchFormRender from "../SearchForm/SearchForm";
+import TrackDescriptionCaptionRender from "../TrackDescriptionCaption/TrackDescriptionCaption";
+import PlayListItemRender from "../PlayList/PlayList";
+import PlayerRender from "../Player/Player";
+import FooterRender from "../Footer/Footer";
+import { PersonalSideBarRender } from "../SideBar/SideBar";
+import {
+  SkeletonTrackRender,SkeletonSideBarRender
+} from "../Skeleton/Skeleton";
+import { dataArray } from "../data";
+import * as S from "./SMain";
+
+function CategoryPageRender() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    // Cancel the timer while unmounting
+    return () => clearTimeout(timer);
+  }, []);
+
+  
+  return (
+    <S.Container>
+      <S.Main>
+        <NavMenuLeftRender />
+        <S.mainCenterblock>
+          <SearchFormRender />
+          <S.H2>Треки</S.H2>
+          <S.centerblockContent>
+            <TrackDescriptionCaptionRender />
+            {loading ? (
+              <SkeletonTrackRender />
+            ) : (
+              dataArray.map((list) => (
+                <PlayListItemRender
+                  key={list.id}
+                  listName={list.name}
+                  listAuthor={list.author}
+                  listAlbum={list.album}
+                  ListDuration_in_seconds={list.duration_in_seconds}
+                />
+              ))
+            )}
+          </S.centerblockContent>
+        </S.mainCenterblock>
+         {loading ? <SkeletonSideBarRender /> : <PersonalSideBarRender userName="Разработчик SkyPro" />}
+      </S.Main>
+      <PlayerRender> {loading}</PlayerRender>
+      <FooterRender />
+    </S.Container>
+  );
+}
+
+export default CategoryPageRender;
